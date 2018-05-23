34704b5bc1f791bdd9a6f380c3549bfa9f9964e2
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-23 07:16:17 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,6 @@ class MP3Importer
   end
   
   def files
-    # .delete_prefix("#{self.path}/") - is better than .sub but I'm not risking updating from 2.3.1 to 2.5.x in the browser IDE
     @files ||= Dir.glob("#{self.path}/*.mp3").collect{ |filename| filename.sub("#{self.path}/", '') }
   end
 
